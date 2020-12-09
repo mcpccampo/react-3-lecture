8b01586eadcc46264e5ab0720cfe704619c9e28d
Camilo Campo <mcp.ccampo@gmail.com>
2020-12-08 23:23:34 -0500

    completed lecture example

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,18 @@
-import React from 'react'
-import './Movie.css'
+import React from 'react';
+import './Movie.css';
 
 function Movie(props) {
-    return (
-        <div className="movie-container">
-            <h3>{props.movie.name}</h3>
-            <p>{props.movie.rating}</p>
-            <p>{props.movie.year}</p>
-            <p>{props.movie.description}</p>
-        </div>
-    )
+  const favorite = props.movie.favorite ? 'favorite' : '';
+  return (
+    <div className={`movie-container ${favorite}`}>
+      <h3>{props.movie.name}</h3>
+      <p>{props.movie.rating}</p>
+      <p>{props.movie.year}</p>
+      <p>{props.movie.description}</p>
+      {/* <button onClick={props.handleFavorite}>Fav?</button> */}
+      <button onClick={() => props.handleFavorite(props.movie.id)}>Fav?</button>
+    </div>
+  );
 }
 
 export default Movie;
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,27 +1,67 @@
-import React from 'react'
-import Movie from './Movie'
-import './MoviesList.css'
-import data from '../data.json'
+import React from 'react';
+import Movie from './Movie';
+import './MoviesList.css';
+import data from '../data.json';
 
 class MoviesList extends React.Component {
-    constructor() {
-        super()
+  constructor() {
+    super();
 
-        this.state = {
-            movies: [],
-            isLoading: true
-        }
+    this.state = {
+      movies: [],
+      isLoading: true,
+    };
+    this.handleFavorite = this.handleFavorite.bind(this);
+  }
+
+  handleFavorite(id) {
+    let changedArray = this.state.movies.map((e) => {
+      if (e.id === id) {
+        return { ...e, favorite: true };
+      } else {
+        return e;
+      }
+    });
+
+    this.setState({
+      movies: changedArray,
+    });
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      console.log('Loading Component Did Mount ..... ');
+      this.setState({
+        movies: data,
+      });
+    }, 2500);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('prevProps', prevProps);
+    console.log('prevState', prevState);
+
+    if (prevState.isLoading) {
+      // you can lso check the prevstate
+      this.setState({
+        isLoading: false,
+      });
     }
+  }
 
-    render() {
-        let mappedMovies = this.state.movies.map(element => <Movie key={element.id} movie={element}/>)
+  componentWillUnmount() {}
 
-        return (
-            <div className="movies-list-container">
-                {mappedMovies}
-            </div>
-        )
-    }
+  render() {
+    let mappedMovies = this.state.movies.map((element) => (
+      <Movie key={element.id} movie={element} handleFavorite={this.handleFavorite} />
+    ));
+
+    return (
+      <div className="movies-list-container">
+        {this.state.isLoading ? 'loading...' : mappedMovies}
+      </div>
+    );
+  }
 }
 
 export default MoviesList;
